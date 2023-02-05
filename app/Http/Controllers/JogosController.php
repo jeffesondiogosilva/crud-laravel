c43a19c565ceebfd81863f65deddf70338cb2acd
Diogo Guedes <diogoworkss@gmail.com>
2023-02-05 18:57:14 -0300

    view jogo criada

--- app/Http/Controllers/JogosController.php
+++ app/Http/Controllers/JogosController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Jogo;
 
 class JogosController extends Controller
 {
     public function index()
     {
+        $jogos = Jogo::all();
+        dd($jogos);
         return view('jogos.index');
 
     }
