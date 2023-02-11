7e2f03a1835cfdf1db6484ee84fd093aba7535fa
Diogo Guedes <diogoworkss@gmail.com>
2023-02-11 16:51:10 -0300

    edit trazendo os dados

--- resources/views/jogos/edit.blade.php
+++ resources/views/jogos/edit.blade.php
@@ -15,18 +15,18 @@
                 </div> <br>
                 <div class="form-group">
                     <label for="categoria">Categoria:</label>
-                    <input type="text" class="form-control" name="categoria" value="" placeholder="digite a categoria">
+                    <input type="text" class="form-control" name="categoria" value="{{ $jogos->categoria }}" placeholder="digite a categoria">
                 </div> <br>
                 <div class="form-group">
                     <label for="ano_criacao">Ano de criação:</label>
-                    <input type="number" class="form-control" name="ano_criacao" value="" placeholder="digite o ano de criação">
+                    <input type="number" class="form-control" name="ano_criacao" value="{{ $jogos->ano_criacao }}" placeholder="digite o ano de criação">
                 </div> <br>
                 <div class="form-group">
                     <label for="valor">Valor:</label>
-                    <input type="number" class="form-control" name="valor" value="" placeholder="digite o valor">
+                    <input type="number" class="form-control" name="valor" value="{{ $jogos->valor }}" placeholder="digite o valor">
                 </div> <br>
                 <div class="form-group">                    
-                    <input type="submit" class="btn btn-primary" name="submit" value="Enviar" >
+                    <input type="submit" class="btn btn-success" name="submit" value="Enviar" >
                 </div>
             </div>
         </form>
