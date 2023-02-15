e5d617e1d1a607e71e4a22fca4d6597f1a9ce289
Diogo Guedes <diogoworkss@gmail.com>
2023-02-14 23:11:25 -0300

    adicionando funcionalidade Serie no crud

---
+++ database/seeders/SeriesSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SeriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Serie::create([
+            'name' => 'Breaking Bad',
+            'categoria' => 'Drama',
+            'ano_criacao' => 2012
+        ]);
+    }
+}
