5b002ba9e3e0ba29c52a82519115d3dea6be30cb
Diogo Guedes <diogoworkss@gmail.com>
2023-02-27 23:31:11 -0300

    View Events criada

--- database/migrations/2023_02_27_010351_create_products_table.php
+++ database/migrations/2023_02_27_010351_create_products_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('crud', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('nome', 55);
-            $table->string('filme-preferido', 55);            
+            $table->string('name', 100);
+            $table->integer('qty');
+            $table->text('description');
             $table->timestamps();
-            
-            
         });
     }
 
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('products');
     }
 };
