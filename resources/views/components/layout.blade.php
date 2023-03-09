2b95d37146c5192d40dbac8efac2ea7bd730381a
Diogo Guedes <diogoworkss@gmail.com>
2023-03-08 21:38:08 -0300

    Edit do menu Series sendo criado

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <title>{{$title}}</title>
-    </head>
-    <body>
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>{{$title}}</title>
+</head>
+
+<body>
 
     <div class="container">
 
-    <h1>{{ $title }}</h1>
-    
-    {{ $slot }}    
-        
+        <h1>{{ $title }}</h1>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+
+        </div>
+        @endif
+        {{ $slot }}
+
     </div>
-    </body>
+</body>
+
 </html>
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,3 +1,3 @@
-<x-layout title="Nova Série">
-    <x-series.form :action="route('series.store')" />
+<x-layout title="Nova Série">    
+    <x-series.form :action="route('series.store')" :nome="old('nome')" :update="false" />
 </x-layout>
